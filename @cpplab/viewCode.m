6b7d0d13ad712b4150ca65a8e7770acc21a81db1
gitlab@srinivas.gs <code@srinivas.gs>
2018-03-26 09:57:02 -0400

    fixed #5

---
+++ @cpplab/viewCode.m
@@ -0,0 +1,11 @@
+%                    _       _     
+%   ___  _     _    | | __ _| |__  
+%  / __|| |_ _| |_  | |/ _` | '_ \ 
+% | (_|_   _|_   _| | | (_| | |_) |
+%  \___||_|   |_|   |_|\__,_|_.__/ 
+%
+% view the underling C++ code
+
+function viewCode(self)
+
+edit(self.cpp_class_path)
