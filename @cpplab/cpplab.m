23e45829fc87e4769b5fb13ecd3210e33e09df5c
gitlab@srinivas.gs <code@srinivas.gs>
2018-08-08 11:23:33 -0400

    better type checks

--- @cpplab/cpplab.m
+++ @cpplab/cpplab.m
@@ -34,9 +34,27 @@ methods
 	% method built in to matlab to strongly type
 	% things that go into the cpplab tree 
 	function self = subsasgn(self, S, value)
-		assert(isscalar(value),['Error assigning value to ' strjoin({S.subs},'.') , ' :: value must be a scalar  '])
-		type_ok = isa(value,'double') || isa(value,'cpplab') || isa(value,'function_handle') || isa(value, 'logical');
-		assert(type_ok, ['Error assigning value to ' strjoin({S.subs},'.') '  :: value is of wrong type'])
+
+		type_ok = true;
+
+		% figure out the lowest dot notation level
+		z = find(strcmp({S.type},'.'),1,'last');
+		if z == 1 & strcmp(class(self),'cpplab')
+			type_ok = isa(value,'double') || isa(value,'cpplab') || isa(value,'function_handle');
+			assert(isscalar(value),['Error assigning value to ' strjoin({S(1:z).subs},'.') , ' :: value must be a scalar  '])
+		elseif z == 1
+			% self is of some dervied class, so anything goes
+			type_ok = true;
+		else
+			% derived class, but we may be indexing into a cpplab object
+			if  strcmp(class(subsref(self,S(1:z-1))),'cpplab')
+				assert(isscalar(value),['Error assigning value to ' strjoin({S(1:z).subs},'.') , ' :: value must be a scalar  '])
+				type_ok = isa(value,'double') || isa(value,'cpplab') || isa(value,'function_handle');
+
+			end
+		end
+		
+		assert(type_ok, ['Error assigning value to ' strjoin({S(1:z).subs},'.') '  :: value is of wrong type'])
 		self = builtin('subsasgn',self,S,value);
 	end
 		
