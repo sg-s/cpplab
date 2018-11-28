9e27054238ffc291c66e7ea292e138207a8119b9
Srinivas Gorur-Shandilya <code@srinivas.gs>
2018-11-27 21:31:53 -0500

    destroy now works

--- @cpplab/destroy.m
+++ @cpplab/destroy.m
@@ -1,6 +1,9 @@
 % destroys a cpplab object within a tree
 
 function destroy(self)
+
+self.parent.Children = setdiff(self.parent.Children,self.dynamic_prop_handle.Name);
+
 delete(self.dynamic_prop_handle)
 
 self.hashSource;
