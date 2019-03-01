36c071b79a8a25d8f5eb14703ef32bfb06e602b0
gitlab@srinivas.gs <code@srinivas.gs>
2019-03-01 16:54:56 -0500

    fixed a bug where C++ files could not be viewed

--- @cpplab/cpplab.m
+++ @cpplab/cpplab.m
@@ -166,7 +166,7 @@ methods (Access = protected)
 			return
 		end
 
-		url = ['matlab:edit(' self.cpp_class_path ')'];
+		url = ['matlab:edit(' char(39) self.cpp_class_path  char(39) ')'];
 		fprintf(['' ' <a href="' url '">' self.cpp_class_name '</a> object (' h(1:7) ') with:\n\n'])
 
 		props = properties(self);
